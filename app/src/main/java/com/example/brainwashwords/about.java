436b205302e50ea7656bab98763dbe3843638c8c
Ori <orish1717@gmail.com>
2025-05-20 01:24:17 +0300

    20/05/2025B

--- app/src/main/java/com/example/brainwashwords/about.java
+++ app/src/main/java/com/example/brainwashwords/about.java
@@ -8,17 +8,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class about extends AppCompatActivity {
-
+public class about extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_about);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_about); // ודא שהשם תואם לקובץ XML שלך
+        setupDrawer(); // אל תשכח לקרוא גם ל־setupDrawer
     }
 }
